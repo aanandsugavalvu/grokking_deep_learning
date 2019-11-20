4751a49e92451b0ae1f2f0902e267fc5e5061f69
AANAND SUGAVALVU <52945905+aanandsugavalvu@users.noreply.github.com>
2019-11-20 19:41:19 +0100

    Update multiple_input_weighted_sum.py
    bug fixes

--- multiple_input_weighted_sum.py
+++ multiple_input_weighted_sum.py
@@ -1,10 +1,13 @@
+#import sys
+
+
 def weighted_sum(data, weights):
     assert (len(data) == len(weights))
 
     output = 0
 
-    for i in range len(data):
-    	output += data(i) * weights(i)
+    for i in range (len(data)):
+    	output += data[i] * weights[i]
 
     return output
 
@@ -15,18 +18,17 @@ def predict(data, weights):
 
 	return predicion
 
-def main():
-	input_toes = [8.5, 10, 23]
-    input_win_pc = [65, 34, 78]
-    input_fans = [12, 8, 15]
 
-    weights = [0.1, 0.2, 0]
+input_toes = [8.5, 10, 23]
+input_win_pc = [65, 34, 78]
+input_fans = [12, 8, 15]
 
-    assert (len(input_toes) == len(input_win_pc) == len(input_fans))
+weights = [0.1, 0.2, 0]
 
-    for i in range(len(input_fans)):
-    	input_vector = []
-    	input_vector.append(input_toes(i), input_win_pc(i), input_fans(i))
-    	predicion = predict(input_vector, weights)
+assert (len(input_toes) == len(input_win_pc) == len(input_fans))
 
-    	print "The chances of team %s winning is %p" % (i +1, predicion,)
+for i in range(len(input_fans)):
+	input_vector = []
+	input_vector.extend([input_toes[i], input_win_pc[i], input_fans[i]])
+	predicion = predict(input_vector, weights)
+	print "The chances of team %s winning is %r" % (i +1, predicion,)
