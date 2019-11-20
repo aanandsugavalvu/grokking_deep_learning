292f96d31679f55d78455c6023a89ceb2bd49a0d
AANAND SUGAVALVU <52945905+aanandsugavalvu@users.noreply.github.com>
2019-11-20 19:30:11 +0100

    Create multiple_input_weighted_sum.py
    added weighted sum method for multiple input parameters

---
+++ multiple_input_weighted_sum.py
@@ -0,0 +1,32 @@
+def weighted_sum(data, weights):
+    assert (len(data) == len(weights))
+
+    output = 0
+
+    for i in range len(data):
+    	output += data(i) * weights(i)
+
+    return output
+
+def predict(data, weights):
+	assert (len(data) == len(weights))
+
+	predicion = weighted_sum(data, weights)
+
+	return predicion
+
+def main():
+	input_toes = [8.5, 10, 23]
+    input_win_pc = [65, 34, 78]
+    input_fans = [12, 8, 15]
+
+    weights = [0.1, 0.2, 0]
+
+    assert (len(input_toes) == len(input_win_pc) == len(input_fans))
+
+    for i in range(len(input_fans)):
+    	input_vector = []
+    	input_vector.append(input_toes(i), input_win_pc(i), input_fans(i))
+    	predicion = predict(input_vector, weights)
+
+    	print "The chances of team %s winning is %p" % (i +1, predicion,)
